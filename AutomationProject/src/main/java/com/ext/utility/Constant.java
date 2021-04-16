a61c1a122ac04eff34bdfd3845b5b56226310d09
patilashish200 <36988223+patilashish200@users.noreply.github.com>
2021-04-16 16:35:50 +0530

    Update Constant.java
    Second commit from Github dierectly

--- AutomationProject/src/main/java/com/ext/utility/Constant.java
+++ AutomationProject/src/main/java/com/ext/utility/Constant.java
@@ -2,7 +2,7 @@ package com.ext.utility;
 
 public class Constant {
  # This is a constant class
-	
+	public final static int col_TCname=100;
 	public final static int col_TCname=0;
 	
 	public final static int col_TCnameval=1;
